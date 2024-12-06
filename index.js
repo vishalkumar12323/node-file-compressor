fb7eb7df6b558e561b1ee3101624aaca6aea3980
vishal <vishalninaniya8949info@gmail.com>
2024-12-06 17:12:44 +0530

    adding multer configration for files

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+import { router } from "./lib/routes/routes.js";
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 // setting up static files serve
 app.use(express.static("./public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
@@ -15,6 +17,7 @@ app.use(cors());
 // setting up view engines (ejs)
 app.set("view engine", "ejs");
 
+app.use(router);
 app.get("/", (req, res) => {
   res.render("index");
 });
